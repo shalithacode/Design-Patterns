7d8cfff6658deb00d887baf97302ad67b2b5342a
shalithacode <shalithalakruwan1998@gmail.com>
2021-03-04 12:41:44 +0530

    loop change

--- Singleton pattern/src/Singleton/Main.java
+++ Singleton pattern/src/Singleton/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Strart...");
-		for(int i = 0; i<10; i++) {
+		for(int i = 0; i<5; i++) {
 			Singleton s = Singleton.getInstance();
 			
 			}

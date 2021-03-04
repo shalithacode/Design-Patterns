c492e611c8fef9454af61f9f3c795876f1579927
shalithacode <shalithalakruwan1998@gmail.com>
2021-03-04 12:31:55 +0530

    2nd

--- Singleton pattern/src/Singleton/Main.java
+++ Singleton pattern/src/Singleton/Main.java
@@ -3,7 +3,7 @@ package Singleton;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Strart");
+		System.out.println("Strart...");
 		for(int i = 0; i<10; i++) {
 			Singleton s = Singleton.getInstance();
 			

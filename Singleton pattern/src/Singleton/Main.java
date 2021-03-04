7bc5a811f64a95d731fc715ba826d6091eaf645f
Shalitha <Shalitha@DESKTOP-P9239DE>
2021-03-04 12:29:29 +0530

    singleton created

--- Singleton pattern/src/Singleton/Main.java
+++ Singleton pattern/src/Singleton/Main.java
@@ -2,4 +2,11 @@ package Singleton;
 
 public class Main {
 
+	public static void main(String[] args) {
+		System.out.println("Strart");
+		for(int i = 0; i<10; i++) {
+			Singleton s = Singleton.getInstance();
+			
+			}
+		}
 }
--- Singleton pattern/src/Singleton/Singleton.java
+++ Singleton pattern/src/Singleton/Singleton.java
@@ -2,4 +2,19 @@ package Singleton;
 
 public class Singleton {
 
+	private static Singleton instance = null;
+
+	public Singleton() {
+		System.out.println("Singleton object created");
+	}
+	
+	public static Singleton getInstance() {
+		
+		if(instance == null) {
+			instance = new Singleton();
+		}
+
+		return instance;
+		
+	}
 }
